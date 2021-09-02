1fdd1267a3d06b1cf8204c9dd8f2744b1b447de0
vkunreal <vkunr3al@gmail.com>
2021-09-02 16:22:44 +0300

    Homework 2 is done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import './App.css';
-import Message from "./components/message";
+import { useState, useEffect } from "react";
+import MessagesList from "./components/MessageList";
+import Form from "./components/Form";
+import "./App.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [sendedMessages, setSendedMessages] = useState(0);
+
+  const sendMessage = (message) => {
+    setMessages([
+      ...messages,
+      {
+        message,
+        author: "You",
+      },
+    ]);
+
+    setSendedMessages((prevValue) => prevValue + 1);
+  };
+
+  const botAnswer = () => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        author: "Bot",
+        message: "Hey, I'm Bot!",
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    if (sendedMessages) {
+      setTimeout(botAnswer, 500);
+    }
+  }, [sendedMessages]);
+
   return (
-    <Message text="Hello world" />
+    <div className="container">
+      <div className="header">
+        <Form onClick={sendMessage} />
+      </div>
+      <MessagesList messages={messages} />
+    </div>
   );
 }
 
